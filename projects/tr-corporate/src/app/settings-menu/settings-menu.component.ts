764b8f4a16ae1f1724b655abb64ca198374df900
Mondal02 <88269183+Mondal02@users.noreply.github.com>
2021-10-19 22:20:45 +0530

    settings fix

---
+++ projects/tr-corporate/src/app/settings-menu/settings-menu.component.ts
@@ -0,0 +1,147 @@
+import { NestedTreeControl } from '@angular/cdk/tree';
+import { Component, OnInit } from '@angular/core';
+import {MatTreeNestedDataSource} from '@angular/material/tree';
+
+interface FoodNode {
+  name: string;
+  route?: string;
+  children?: FoodNode[];
+  icon?: string;
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Permission',
+    children: [
+      {name: 'User Management'},
+      {name: 'Account Role'},
+    ],
+    icon: 'search',
+  }, {
+    name: 'Templates',
+    children: [
+      {name: 'User Management'},
+      {name: 'Account Role'},
+    ],
+    icon: 'table_view',
+  },{
+    name: 'Integration',
+    children: [
+      {name: 'User Management'},
+      {name: 'Account Role'},
+    ],
+    icon: 'done_outline',
+  }, {
+    name: 'Configuration',
+    children: [
+      {name: 'User Management'},
+      {name: 'Account Role'},
+    ],
+    icon: 'reorder',
+  },{
+    name: 'Permission',
+    children: [
+      {name: 'User Management'},
+      {name: 'Account Role'},
+    ],
+    icon: 'search',
+  }, {
+    name: 'Templates',
+    children: [
+      {name: 'User Management'},
+      {name: 'Account Role'},
+    ],
+    icon: 'table_view',
+  },{
+    name: 'Integration',
+    children: [
+      {name: 'User Management'},
+      {name: 'Account Role'},
+    ],
+    icon: 'done_outline',
+  }, {
+    name: 'Configuration',
+    children: [
+      {name: 'User Management'},
+      {name: 'Account Role'},
+    ],
+    icon: 'reorder',
+  },{
+    name: 'Permission',
+    children: [
+      {name: 'User Management'},
+      {name: 'Account Role'},
+    ],
+    icon: 'search',
+  }, {
+    name: 'Templates',
+    children: [
+      {name: 'User Management'},
+      {name: 'Account Role'},
+    ],
+    icon: 'table_view',
+  },{
+    name: 'Integration',
+    children: [
+      {name: 'User Management'},
+      {name: 'Account Role'},
+    ],
+    icon: 'done_outline',
+  }, {
+    name: 'Configuration',
+    children: [
+      {name: 'User Management'},
+      {name: 'Account Role'},
+    ],
+    icon: 'reorder',
+  },{
+    name: 'Permission',
+    children: [
+      {name: 'User Management'},
+      {name: 'Account Role'},
+    ],
+    icon: 'search',
+  }, {
+    name: 'Templates',
+    children: [
+      {name: 'User Management'},
+      {name: 'Account Role'},
+    ],
+    icon: 'table_view',
+  },{
+    name: 'Integration',
+    children: [
+      {name: 'User Management'},
+      {name: 'Account Role'},
+    ],
+    icon: 'done_outline',
+  }, {
+    name: 'Configuration',
+    children: [
+      {name: 'User Management'},
+      {name: 'Account Role'},
+    ],
+    icon: 'reorder',
+  },
+];
+
+@Component({
+  selector: 'app-settings-menu',
+  templateUrl: './settings-menu.component.html',
+  styleUrls: ['./settings-menu.component.scss']
+})
+export class SettingsMenuComponent implements OnInit {
+
+  treeControl = new NestedTreeControl<FoodNode>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<FoodNode>();
+
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
+
+  hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
+  
+  ngOnInit(): void {
+  }
+
+}
