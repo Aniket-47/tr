789d9442a96c0312f7d0af7627df9f50903b7249
Aniket-2512 <Aniket.d@mucrest.com>
2021-10-22 15:31:41 +0530

    routing config fixed

--- projects/tr-corporate/src/app/settings/permission/permission-routing.module.ts
+++ projects/tr-corporate/src/app/settings/permission/permission-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddRoleComponent } from './add-role/add-role.component';
 import { MFilterComponent } from './m-filter/m-filter.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 import { UserManageComponent } from './user-manage/user-manage.component';
 import { ViewRoleComponent } from './view-role/view-role.component';
 
@@ -9,6 +10,10 @@ const routes: Routes = [
   {
     path:'users',
     component: UserManageComponent
+  },
+  {
+    path:'usersdetails',
+    component: UserDetailComponent
   },
   {
     path:'roles',
