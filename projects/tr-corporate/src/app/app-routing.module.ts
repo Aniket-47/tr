2bd27a52a3d23e715239a58f99fcf4fb841e1b58
atif619 <sheikhatib39@gmail.com>
2021-10-22 20:57:41 +0530

    fixed stying again

--- projects/tr-corporate/src/app/app-routing.module.ts
+++ projects/tr-corporate/src/app/app-routing.module.ts
@@ -1,23 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-import { AuthModule } from './auth/auth.module';
-import { UserDetailComponent } from './user-detail/user-detail.component';
-import { SettingsMenuComponent } from './settings-menu/settings-menu.component';
-
-const routes: Routes = [{
-  path: 'auth',
-  loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
-
-},{
-  path : 'userdetail',
-  component : UserDetailComponent
-},
-{
-  path: 'settings',
-  component: SettingsMenuComponent
-}
-=======
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
@@ -44,7 +26,6 @@ const routes: Routes = [
       }
     ]
   }
->>>>>>> 23797a001d30d616aa5d6f3c711b7843dac201da
 ];
 
 @NgModule({
--- projects/tr-corporate/src/app/app.module.ts
+++ projects/tr-corporate/src/app/app.module.ts
@@ -5,32 +5,12 @@ import { MaterialModule } from '@tr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { AuthModule } from './auth/auth.module';
-import { UserDetailComponent } from './user-detail/user-detail.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatSelectModule} from '@angular/material/select';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatInputModule} from '@angular/material/input';
-
-import { SettingsMenuComponent } from './settings-menu/settings-menu.component';
-import { MatTreeModule } from '@angular/material/tree';
-import { MatFormFieldModule } from '@angular/material/form-field';
-
-=======
-// import { AuthModule } from './auth/auth.module';
->>>>>>> 23797a001d30d616aa5d6f3c711b7843dac201da
+// import { MaterialModule } from './material.moule';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    UserDetailComponent,
-    SettingsMenuComponent
-
-=======
->>>>>>> 23797a001d30d616aa5d6f3c711b7843dac201da
   ],
   imports: [
     BrowserModule,
@@ -38,21 +18,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     // TRModule,
     // AuthModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    MatButtonModule,
-    MatIconModule,
-    MatMenuModule,
-    MatSelectModule,
-
-// mat imports
-    MatInputModule,
-    MatTreeModule,
-    MatIconModule,
-    MatFormFieldModule,
-    MatButtonModule
-=======
     MaterialModule
->>>>>>> 23797a001d30d616aa5d6f3c711b7843dac201da
   ],
   providers: [],
   bootstrap: [AppComponent]
