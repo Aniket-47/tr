4a047bf030e6ec2726784811addb3aa3d5695e7c
Aniket-2512 <Aniket.d@mucrest.com>
2021-10-28 14:05:57 +0530

    api service added

--- projects/tr-corporate/src/app/utility/apiConfig.ts
+++ projects/tr-corporate/src/app/utility/apiConfig.ts
@@ -2,17 +2,17 @@ import { environment } from './../../environments/environment';
 
 
 export const api_routes = {
-  REGISTER: '${environment.authApiBasePath}/registration',
-  VERIFICATION: '${environment.authApiBasePath}/accountverification',
-  VALIDATE_EMAIL: '${environment.authApiBasePath}/validateemail',
-  LOGIN: '${environment.authApiBasePath}/login'
+  REGISTER: `${environment.authApiBasePath}/registration`,
+  VERIFICATION: `${environment.authApiBasePath}/accountverification`,
+  VALIDATE_EMAIL: `${environment.authApiBasePath}/validateemail`,
+  LOGIN: `${environment.authApiBasePath}/login`
 };
 
 export const secure_api_routes = {
-  CHANGE_PASSWORD: '${environment.authApiBasePath}/changepassword',
-  REFRESH_TOKEN: '${environment.authApiBasePath}/token/$refreshToken',
-  LOGOUT_ALL: '${environment.authApiBasePath}/logout/all',
-  RESET_PASSWORD: '${environment.authApiBasePath}/resetpassword/$resetPassToken',
-  FORGOT_PASSWORD: '${environment.authApiBasePath}/forgotpassword',
-  LOGOUT: '${environment.authApiBasePath}/logout'
+  CHANGE_PASSWORD: `${environment.authApiBasePath}/changepassword`,
+  REFRESH_TOKEN: `${environment.authApiBasePath}/token/$refreshToken`,
+  LOGOUT_ALL: `${environment.authApiBasePath}/logout/all`,
+  RESET_PASSWORD: `${environment.authApiBasePath}/resetpassword/$resetPassToken`,
+  FORGOT_PASSWORD: `${environment.authApiBasePath}/forgotpassword`,
+  LOGOUT: `${environment.authApiBasePath}/logout`
 };
