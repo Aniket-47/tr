591a10bf1c8ffc981f945acb427a983cdf43d0e6
Saikat Hajra <saikathajra@Saikats-MacBook-Air.local>
2021-10-28 10:15:24 +0530

    pwa added

--- projects/tr-corporate/src/app/app.module.ts
+++ projects/tr-corporate/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AdvanceSearchModule } from './advance-search/advance-search.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -19,6 +21,12 @@ import { AppComponent } from './app.component';
     MaterialModule,
     AdvanceSearchModule,
     AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
