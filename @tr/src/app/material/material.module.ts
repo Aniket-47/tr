6c33432150b7d1edb17bdc3f71108a81801855fd
Mondal02 <88269183+Mondal02@users.noreply.github.com>
2021-10-29 11:28:16 +0530

    Added animation on search panel

--- @tr/src/app/material/material.module.ts
+++ @tr/src/app/material/material.module.ts
@@ -43,6 +43,9 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+
 const matModules = [
   MatAutocompleteModule,
   MatCheckboxModule,
@@ -78,7 +81,8 @@ const matModules = [
   MatTooltipModule,
   MatPaginatorModule,
   MatSortModule,
-  MatTableModule
+  MatTableModule,
+  BrowserAnimationsModule,
 ]
 @NgModule({
   declarations: [],
