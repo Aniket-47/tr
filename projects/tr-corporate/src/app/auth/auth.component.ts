00467b49b96a44312e67fdf07d7a8d7b11a25b78
uideveloperdk <uideveloperdk@gmail.com>
2021-10-29 23:10:39 +0530

    ui fix and manage profile component create and routing

--- projects/tr-corporate/src/app/auth/auth.component.ts
+++ projects/tr-corporate/src/app/auth/auth.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
-  stepperPages = ['Company Type', 'Register'];
+  stepperPages = ['Company Type', 'Register', 'Login'];
   currentStep = 1;
 
   leftPanelImg: string;
