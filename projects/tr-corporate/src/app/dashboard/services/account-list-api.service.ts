2785bdf65aa60b6b81b6d4ff63573507c1c2104f
Aniket-2512 <Aniket.d@mucrest.com>
2021-11-01 16:19:08 +0530

    config & service added

---
+++ projects/tr-corporate/src/app/dashboard/services/account-list-api.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+// Utility
+import { UtilityService } from '../../utility/services/utility';
+import { api_routes, secure_api_routes } from '../../utility/configs/apiConfig';
+
+// Interfaces
+import { AccountList_response } from '../interfaces/account-list';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountListApiService {
+  api_routes;
+  secure_api_routes;
+  constructor(private http: HttpClient, private utilityServ: UtilityService) {
+    this.api_routes = api_routes;
+    this.secure_api_routes = secure_api_routes;
+  }
+
+
+  getAccountList() {
+    return this.http.get<AccountList_response>(secure_api_routes.ACCOUNT_LIST)
+  }
+}
--- projects/tr-corporate/src/app/utility/configs/apiConfig.ts
+++ projects/tr-corporate/src/app/utility/configs/apiConfig.ts
@@ -4,6 +4,7 @@ const basePath = environment.apiDomain + '/api/v1';
 const auth = basePath + '/auth';
 
 export const api_routes = {
+  // auth module
   REGISTER: `${auth}/registration`,
   VERIFICATION: `${auth}/accountverification`,
   VALIDATE_EMAIL: `${auth}/validateemail`,
@@ -12,9 +13,13 @@ export const api_routes = {
 };
 
 export const secure_api_routes = {
+  // auth module
   CHANGE_PASSWORD: `${auth}/changepassword`,
   REFRESH_TOKEN: `${auth}/token/$refreshToken`,
   LOGOUT_ALL: `${auth}/logout/all`,
   RESET_PASSWORD: `${auth}/resetpassword/$resetPassToken`,
-  LOGOUT: `${auth}/logout`
+  LOGOUT: `${auth}/logout`,
+
+  // permission
+  ACCOUNT_LIST: `${auth}/user/account`
 };
