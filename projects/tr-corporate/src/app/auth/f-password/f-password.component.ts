090423eaca3dadfd202bbe053b8838d27db94c70
Aniket-2512 <Aniket.d@mucrest.com>
2021-11-03 14:13:42 +0530

    input validation

--- projects/tr-corporate/src/app/auth/f-password/f-password.component.ts
+++ projects/tr-corporate/src/app/auth/f-password/f-password.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 
@@ -8,34 +8,40 @@ import { AuthService } from '../services/auth.service';
   templateUrl: './f-password.component.html',
   styleUrls: ['./f-password.component.scss']
 })
-export class FPasswordComponent implements OnInit{
+export class FPasswordComponent implements OnInit {
 
-  resMessage: string="";
+  resMessage: string = "";
   isLoading = false;
   isError = false;
 
   constructor(private authServ: AuthService) { }
 
   ngOnInit(): void {
-  }  
+  }
 
-  emailFormControl=new FormControl('',[
+  emailFormControl = new FormControl('', [
     Validators.required,
     Validators.email
   ])
 
   fpassword() {
-    this.resMessage = "";
-    this.isLoading = true;
-    this.emailFormControl.valid && this.authServ.passwordForget(this.emailFormControl.value)
-      .subscribe((res: any) => {
-        this.isLoading = false;
-       
-        this.isError = res.error==='true'?true:false;
-        this.emailFormControl.setErrors({'customError': true})
-        this.resMessage=res.message;        
-      })
+
+    if (this.emailFormControl.valid) {
+      this.resMessage = "";
+      this.isLoading = true;
+      this.emailFormControl.markAsTouched();
+
+      this.authServ.passwordForget(this.emailFormControl.value)
+        .subscribe(res => {
+          this.isLoading = false;
+
+          this.isError = res.error;
+          this.emailFormControl.setErrors({ 'customError': true })
+          this.resMessage = res.message;
+        })
+    }
+
   }
 
-  
+
 }
