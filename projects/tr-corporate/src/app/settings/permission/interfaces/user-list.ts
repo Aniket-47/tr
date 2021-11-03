a93c0b1466b7245975f754e540ad2d3324f787f6
Aniket-2512 <Aniket.d@mucrest.com>
2021-11-03 21:18:31 +0530

    test

--- projects/tr-corporate/src/app/settings/permission/interfaces/user-list.ts
+++ projects/tr-corporate/src/app/settings/permission/interfaces/user-list.ts
@@ -9,7 +9,7 @@ export interface UserList_request {
       role: "TXT_ROLE_INTERVIEWPANELIST",
       email: "aniket.d@mucrest.com",
       Status: 1,
-      lastUpdated: "2021-11-02T16:23:22.000Z"
+      last_updated: "2021-11-02T16:23:22.000Z"
     }
   ]
 
