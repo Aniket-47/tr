0266d3fdb7e28be00295b02162f50bbaf0223fb7
Aniket-2512 <Aniket.d@mucrest.com>
2021-11-15 21:27:27 +0530

    mobile view fixes

--- projects/tr-corporate/src/app/settings/permission/m-filter/m-filter.component.ts
+++ projects/tr-corporate/src/app/settings/permission/m-filter/m-filter.component.ts
@@ -1,5 +1,8 @@
+import { getRoles } from './../../../utility/store/selectors/roles.selector';
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { fadeAnimation } from '../../../animations';
+import { State } from '../../../utility/store/reducers';
 
 @Component({
   selector: 'app-m-filter',
@@ -9,13 +12,20 @@ import { fadeAnimation } from '../../../animations';
 })
 export class MFilterComponent implements OnInit {
 
-  typesOfShoes: string[] = ['Sort by Name', 'Sort by Status', 'Sort by Role'];
-  typesOfShoes1: string[] = ['Admin', 'Super Admin', 'Relationship Manager'];
-  typesOfShoes2: string[] = ['All', 'Active', 'Deactivated'];
-  
-  constructor() { }
+  sortTypes: string[] = ['Sort by Status', 'Sort by Role'];
+  status = [
+    { value: '0', viewValue: 'Deactive' },
+    { value: '1', viewValue: 'Active' },
+    { value: '2', viewValue: 'Pending' }
+  ];
+  role!: any[];
+
+  constructor(private store: Store<State>) { }
 
   ngOnInit(): void {
+    this.store.select(getRoles).subscribe(roles => {
+      this.role = roles;
+    });
   }
 
 }
