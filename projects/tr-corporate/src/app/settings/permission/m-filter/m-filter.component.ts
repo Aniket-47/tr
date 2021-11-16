8bea8f9b630071549b3285cad9ebfdc3b1664efc
sudiptokoyal <sudipto.k@mucrest.com>
2021-11-16 10:23:33 +0530

    fixed mobile filter

--- projects/tr-corporate/src/app/settings/permission/m-filter/m-filter.component.ts
+++ projects/tr-corporate/src/app/settings/permission/m-filter/m-filter.component.ts
@@ -1,10 +1,10 @@
 import { getRoles } from './../../../utility/store/selectors/roles.selector';
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { fadeAnimation } from '../../../animations';
 import { State } from '../../../utility/store/reducers';
 import { MatDialogRef } from '@angular/material/dialog';
-import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
+import { MatBottomSheetRef, MAT_BOTTOM_SHEET_DATA } from '@angular/material/bottom-sheet';
 import { FilterService } from '../services/filter.service';
 
 @Component({
@@ -14,7 +14,7 @@ import { FilterService } from '../services/filter.service';
   animations: [fadeAnimation]
 })
 export class MFilterComponent implements OnInit {
-
+  // Filter data
   sortTypes = [
     { value: 'status', viewValue: 'Sort by Status' },
     { value: 'roletypeid', viewValue: 'Sort by Role' }
@@ -26,34 +26,45 @@ export class MFilterComponent implements OnInit {
   ];
   role!: any[];
 
-  selectedData!: {
-    sort: string,
-    sortOrder: string,
-    filter_roletypeid: number,
-    filter_status: number
-  };
 
+  // Applied filter data
   selectedSort!: string;
   selectedStatus!: number;
   selectedRole!: number;
 
-  constructor(private store: Store<State>, public dialogRef: MatBottomSheetRef<MFilterComponent>, public filterserv: FilterService) {
-
+  constructor(
+    @Inject(MAT_BOTTOM_SHEET_DATA) public data: any,
+    private store: Store<State>,
+    public bottomsheetRef: MatBottomSheetRef<MFilterComponent>,
+    public filterserv: FilterService) {
   }
 
   ngOnInit(): void {
-    this.store.select(getRoles).subscribe(roles => {
-      this.role = roles;
-    });
+    this.store.select(getRoles).subscribe(roles => this.role = roles);
+
+    if (this.data) {
+      if (this.data.sort) this.selectedSort = this.data.sort;
+      if (this.data.filter_roletypeid) this.selectedRole = this.data.filter_roletypeid;
+      if (this.data.filter_status) this.selectedStatus = this.data.filter_status;
+    }
   }
 
+  dismiss(fallbckData: any = null) {
+    this.bottomsheetRef.dismiss(fallbckData);
+  }
 
   onfilter() {
-    this.filterserv.SelectedRole = this.selectedRole;
-    this.filterserv.SelectedSort = this.selectedSort;
-    this.filterserv.SelectedStatus = this.selectedStatus;
-    // this.dialogRef.afterDismissed({ 'sort': this.selectedSort, 'filter_roletypeid': this.selectedRole, 'filter_status': this.selectedStatus })
-    this.dialogRef.dismiss()
+    // this.filterserv.SelectedRole = this.selectedRole;
+    // this.filterserv.SelectedSort = this.selectedSort;
+    // this.filterserv.SelectedStatus = this.selectedStatus;
+    // this.bottomsheetRef.afterDismissed({ 'sort': this.selectedSort, 'filter_roletypeid': this.selectedRole, 'filter_status': this.selectedStatus })
+    const filterData = { 'sort': this.selectedSort, 'filter_roletypeid': this.selectedRole, 'filter_status': this.selectedStatus }
+    this.dismiss(filterData)
+  }
+
+  onReset() {
+    const filterData = { sort: null, filter_roletypeid: null, filter_status: null }
+    this.dismiss(filterData);
   }
 
 }
