c588533b44accb203513a0a970bbb65b1f02c9ca
Aniket-2512 <Aniket.d@mucrest.com>
2021-11-17 12:43:43 +0530

    language api integration structure

---
+++ projects/tr-corporate/src/app/dashboard/shared/services/account-list-api.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+// Utility
+import { api_routes, secure_api_routes } from '../../../utility/configs/apiConfig';
+import { UtilityService } from '../../../utility/services/utility.service';
+
+// Interfaces
+import { AccountList_response } from '../../shared/interfaces/account-list';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountListApiService {
+  private api_routes;
+  private secure_api_routes;
+  constructor(private http: HttpClient, private utilityServ: UtilityService) {
+    this.api_routes = api_routes;
+    this.secure_api_routes = secure_api_routes;
+  }
+
+
+  getAccountList() {
+    return this.http.get<AccountList_response>(secure_api_routes.ACCOUNT_LIST)
+  }
+}
