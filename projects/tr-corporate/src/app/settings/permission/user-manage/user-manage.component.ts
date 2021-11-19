24b972879533da3f102d5f07ce20ae7b6c0a4726
Mondal02 <88269183+Mondal02@users.noreply.github.com>
2021-11-19 15:42:56 +0530

    Added text indentifiers and keys

--- projects/tr-corporate/src/app/settings/permission/user-manage/user-manage.component.ts
+++ projects/tr-corporate/src/app/settings/permission/user-manage/user-manage.component.ts
@@ -85,7 +85,7 @@ export class UserManageComponent implements OnInit {
 
   ngOnInit(): void {
     this.store.select(getRoles).subscribe(roles => {
-      this.role = [{ roletypeid: '', name: 'All' }, ...roles];
+      this.role = [{ roletypeid: '', name: this.ln.TXT_ALL}, ...roles];
     });
   }
 
