9847daf92f16506333f9e66a0821eabfa3f86ee3
Aniket-2512 <Aniket.d@mucrest.com>
2021-11-23 13:59:23 +0530

    hotfix

--- projects/tr-corporate/src/app/utility/services/snack-bar.service.ts
+++ projects/tr-corporate/src/app/utility/services/snack-bar.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { TranslatePipe } from '@mucrest/ng-core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SnackBarService {
 
-  constructor(private snackBar: MatSnackBar) { }
+  constructor(private snackBar: MatSnackBar, private translate: TranslatePipe) { }
   open(message: string, action?: string, duration: number = 4000, className?: string) {
-    this.snackBar.open(message, action, { duration })
+    this.snackBar.open(message, this.translate.transform(action || ''), { duration })
   }
 }
